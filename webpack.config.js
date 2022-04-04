5bc20504b43328fbb09a0ef577d3e0f51eccd265
kdingman <kcdonlan@yahoo.com>
2022-04-04 15:49:36 -0400

    modularized code into smaller bundles, separated by concern or purpose

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,15 @@ const webpack = require("webpack");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: "[name].bundle.js", // name used in entry object will be used in place of [name] in each bundle.js file that is created.
+        path: __dirname + "/dist", // output bundle files will be written to the dist folder
     },
     plugins: [
         new webpack.ProvidePlugin({
